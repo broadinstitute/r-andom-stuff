04929729af83e4eba636c5a34103cba367df26c9
curoli <oliverr@broadinstitute.org>
2024-08-13 14:02:41 -0400

    Allow y_min/y_max to be set with plot.cols. Also, more R-ish function names.

--- andom.R
+++ andom.R
@@ -9,19 +9,29 @@ mean3 <- function(data) {
   (c(data[2:n], last) + data + c(first, data[1:(n - 1)])) / 3
 }
 
-mean3_df <- function(data) {
+# Apply smooting to all values in a data frame
+mean3.df <- function(data) {
   data.frame(lapply(data, mean3))
 }
 
-plot_cols <- function(data) {
+# Plot all values in a data frame as lines over index
+plot.cols <- function(data, y_min = NULL, y_max = NULL) {
   col1 <- data[,1]
   x_min <- 1
   x_max <- length(col1)
-  y_min <- min(col1)
-  y_max <- max(col1)
-  for (col in data[-1]) {
-    y_min <- min(y_min, min(col))
-    y_max <- max(y_max, max(col))
+  if (is.null(y_min) || is.null(y_max)) {
+    y_min_auto <- min(col1)
+    y_max_auto <- max(col1)
+    for (col in data[-1]) {
+      y_min_auto <- min(y_min_auto, min(col))
+      y_max_auto <- max(y_max_auto, max(col))
+    }
+    if (is.null(y_min)) {
+      y_min <- y_min_auto
+    }
+    if (is.null(y_max)) {
+      y_max <- y_max_auto
+    }
   }
   plot(0, 0, xlim = c(x_min, x_max), ylim = c(y_min, y_max), type = "n")
   for (i in 1:(length(data))) {
